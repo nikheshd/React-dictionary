7cf3304441719c3fd820df372d32cb9c831821b4
nikheshd <nikheshd@iitk.ac.in>
2023-01-18 00:42:47 +0530

    .

--- src/components/TabItems/index.js
+++ src/components/TabItems/index.js
@@ -1,5 +1,11 @@
 import './index.css'
 
+//https://stackoverflow.com/questions/41768215/english-json-dictionary-with-word-word-type-and-definition
+// The dictionary is arranged in this format- {WORD: {'MEANINGS':{} , 'ANTONYMS':[...] , 'SYNONYMS':[...]}}
+
+// and the 'MEANINGS' dict is arranged as 'MEANINGS':{sense_num_1:[TYPE_1, MEANING_1, CONTEXT_1, EXAMPLES], sense_num_2:[TYPE_2, MEANING_2, CONTEXT_2, EXAMPLES] and so on...}
+
+
 const TabItem = props => {
   const {tabDetails} = props
   // const {id, timeAccessed, logoUrl, title, domainUrl} = tabDetails
